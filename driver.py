712187426d6a918103dff145a1ae4eda9b9e97f6
TinhLa72 <tla005@ucr.edu>
2021-05-18 14:09:52 -0700

    create menu and add .gitignore

--- driver.py
+++ driver.py
@@ -1,3 +1,11 @@
 from node import *
 from forward import *
 from backward import *
+
+print("Welcome to 861126014 (Michael Chiang) & 861199635 (Tinh La) group's Feature Selection Algorithm.\n")
+numFeatures = input("Please enter total number of features: ")
+print("\n") #Empty line
+print("Type the number of the algorithm you want to run: ")
+print("(1) - Forward Selection\n(2) Backward Elimination\n")
+algoChoice = input("Choice: ")
+#run algo
