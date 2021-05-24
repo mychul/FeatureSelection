ff5873fbbb513608c04db9e0f7d7c9c2a3f34058
Michael Chiang <mychiang13@gmail.com>
2021-05-23 20:28:29 -0700

    fixed bug with 1 feature

--- evaluate.py
+++ evaluate.py
@@ -8,11 +8,10 @@ class evaluate:
     def randoList(self,frontier):
         self.evaluated.clear()
         for x in frontier:
-            print (x)
             ran = random.randint(0,100)
             self.evaluated.append((ran,x))
             #trace output
         for y in self.evaluated:
-            print("Using feature(s) " + str(y[1]) + " accuracy is "+ str(y[0]))
+            print("Using feature(s) " + str(list(y[1])) + " accuracy is "+ str(y[0]))
         return max(self.evaluated)
 
