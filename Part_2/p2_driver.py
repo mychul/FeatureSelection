9505f18ccf7406f24324cb52ed22aa4ac7e67644
Michael Chiang <mychiang13@gmail.com>
2021-06-12 14:12:13 -0700

    Update p2_driver.py

--- Part_2/p2_driver.py
+++ Part_2/p2_driver.py
@@ -4,7 +4,7 @@ import time
 
 print("Welcome to 861126014 (Michael Chiang) & 861199635 (Tinh La) group's part 2 for feature selection project.\n")
 tic=time.perf_counter()
-subset=(1,3,4,6,9)
+subset=(2,4)
 small_classify = Classifier()
 small_classify.Train()
 valid = Validator(small_classify)
